c11559b9a11466e9b011f5e5ecaa1953a1ed4433
Júlia Marques <jumarques631@gmail.com>
2025-06-21 16:42:24 -0300

    Update main.py
    Criei a função responsável pela obtenção do status do inversor. Adicionei tratamento de erro também!

--- backend/main.py
+++ backend/main.py
@@ -3,26 +3,35 @@ from fastapi.responses import JSONResponse
 
 app=FastAPI()
 
-@app.post("status-inversor")
-async def saber_status_inversor(request: Request):
-    corpo_intent= await request.json()  # Espera receber o corpo do intent
-    intent=corpo_intent["request"]["intent"]["name"]    # Acessando o nome do intent requerido pelo usuário
-
-    if intent== "StatusInversor":
-        texto_resposta = "Status Inversor...."    # Aqui ocorreria uma requisição para a API da Goodwe referente ao status do inversor
-    else:
-        texto_resposta= "Desculpe, não entendi sua solicitação! Poderia repetir por favor?"
-
-    # Resposta no formato que a Alexa espera receber
+def corpo_resposta_para_Alexa(texto, acabar_sessao):
     resposta = {
-        "version": "1.0",
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": texto_resposta
-            },
-            "shouldEndSession": False
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": texto
+                },
+                "shouldEndSession": acabar_sessao
+            }
         }
-    }
+    return resposta
 
-    return JSONResponse(content=resposta)   # Retorna uma resposta HTTP no formato JSON
+@app.post("/status-inversor")
+async def saber_status_inversor(request: Request):
+    try: 
+        corpo_intent= await request.json()  # Espera receber o corpo do intent
+        intent=corpo_intent["request"]["intent"]["name"]    # Acessando o nome do intent requerido pelo usuário
+
+        if intent== "StatusInversor":
+            texto_resposta = "Status Inversor...."    # Aqui ocorreria uma requisição para a API da Goodwe referente ao status do inversor
+        else:
+            texto_resposta= "Desculpe, não entendi sua solicitação! Poderia repetir por favor?"
+
+        # Resposta no formato que a Alexa espera receber
+        resposta=corpo_resposta_para_Alexa(texto_resposta, False)
+        return JSONResponse(content=resposta, status_code=200)   # Retorna uma resposta HTTP no formato JSON
+    
+    except Exception as e:
+        print(e)
+        resposta_erro=corpo_resposta_para_Alexa("Desculpe, houve um problema ao processar sua solicitação.", True)
+        return JSONResponse(content=resposta_erro, status_code=200) 
