cd8f365eb087fffc37951122f79f66207d05faa3
Júlia Marques <jumarques631@gmail.com>
2025-08-18 16:22:17 -0300

    Update status_inversor_simulado.py

--- simulacoes/status_inversor_simulado.py
+++ simulacoes/status_inversor_simulado.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import random
 import openpyxl
-from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 def info_inversor():
-    caminho_base = Path(__file__).resolve().parent.parent
-    caminho_arquivo = caminho_base / "dados" / "dados_finais_inversor.xlsx"
-    dados= pd.read_excel(caminho_arquivo)
+    load_dotenv()
+    caminho = os.getenv("CAMINHO")
+    dados= pd.read_excel(caminho)
 
     dicionario_dados = dados.to_dict(orient="records")
     n = random.randint(0, len(dicionario_dados) - 1)
