9c41f8ac95cf2bf6346b0f368565a42eb83d7f75
Júlia Marques <jumarques631@gmail.com>
2025-09-05 17:57:34 -0300

    arrumando graficos

--- backend/site/rotas.py
+++ backend/site/rotas.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter 
 from simulacoes.status_aparelhos import infos
 from backend.funcs_auxiliares.funcs_auxiliares import ler_cargas, salvar_cargas_prioritarias, reorganizar_indices, obter_clima
-from backend.graficos.graficos import serie_temporal, histograma, grafico_bateria
+from backend.graficos.graficos import serie_temporal, histograma
 from backend.IA.llm import assistente_llm_site
 
 rota_site= APIRouter(prefix="/site")
@@ -74,7 +74,7 @@ async def obter_historico_de_consumo():
 @rota_site.get("/dados_bateria")
 async def obter_historico_de_consumo():
     try:
-        dados_bateria_grafico= grafico_bateria()
+        dados_bateria_grafico=serie_temporal('Dados da Bateria(W)','orange', 'Uso da Bateria(W) por Dia no Mês de Agosto', 'Dia', 'Watts')
         return dados_bateria_grafico
     except:
         return {"mensagem":"Não foi possível o gráfico!"}
